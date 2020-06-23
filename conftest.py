16f59915df9ca5546f2fbc2b98d56e7d930a68e7
Rudiger Wolf <Rudiger.Wolf@ThroughputFocus.com>
2020-06-23 20:10:14 +0100

    Changed image diff to https://github.com/nicolashahn/diffimg

--- conftest.py
+++ conftest.py
@@ -12,27 +12,29 @@ import pytest
 import numpy as np
 from PIL import Image
 import platform
-
+from diffimg import diff
 
 def assert_images_equal(image_1: str, image_2: str):
     """ Compare if two PNG images are nearly similar. """
-    img1 = Image.open(image_1)
-    img2 = Image.open(image_2)
+    #img1 = Image.open(image_1)
+    #img2 = Image.open(image_2)
 
     # Convert to same mode and size for comparison
-    img2 = img2.convert(img1.mode)
-    img2 = img2.resize(img1.size)
+    #img2 = img2.convert(img1.mode)
+    #img2 = img2.resize(img1.size)
 
-    sum_sq_diff = np.sum(
-        (np.asarray(img1).astype("float") - np.asarray(img2).astype("float")) ** 2,
-    )
+    #sum_sq_diff = np.sum(
+    #    (np.asarray(img1).astype("float") - np.asarray(img2).astype("float")) ** 2,
+    #)
+    percent_diff = diff(image_1,image_2,delete_diff_file=True,ignore_alpha=False)
 
-    if sum_sq_diff == 0:
-        # Images are exactly the same
+    if percent_diff <= 5.0:
+        # Images are almost the same
         pass
     else:
-        normalized_sum_sq_diff = sum_sq_diff / np.sqrt(sum_sq_diff)
-        assert normalized_sum_sq_diff < 0.001
+        #normalized_sum_sq_diff = sum_sq_diff / np.sqrt(sum_sq_diff)
+        #assert normalized_sum_sq_diff < 0.001
+        assert percent_diff < 5.0
 
 
 @pytest.fixture
